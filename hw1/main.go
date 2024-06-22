3a281dcadb8d839d7a27ff4e0d30fd82ffcb0cd7
Matvey <enhisir@localhost.localdomain>
2024-06-22 16:11:41 +0300

    task 1 done

--- hw1/main.go
+++ hw1/main.go
@@ -20,3 +20,68 @@ func main() {
 		panic(err.Error())
 	}
 }
+
+func dirTree(out io.Writer, path string, printFiles bool) error {
+	return dirTreeRecursive(out, path, printFiles, make([]bool, 0))
+}
+
+func dirTreeRecursive(out io.Writer, path string, printFiles bool, levels []bool) error {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return err
+	}
+
+	if !printFiles {
+		entries = filter(entries, func(e os.DirEntry) bool { return e.IsDir() })
+	}
+
+	levelsLength := len(levels)
+
+	var prefixBuilder strings.Builder
+	for _, printStick := range levels {
+		if printStick {
+			prefixBuilder.WriteRune('│')
+		}
+		prefixBuilder.WriteRune('\t')
+	}
+	preparedPrefix := prefixBuilder.String()
+
+	newLevels := append(levels, true)
+	entriesLength := len(entries)
+	for idx, e := range entries {
+		hinge := '├'
+		if idx == entriesLength-1 {
+			hinge = '└'
+			newLevels[levelsLength] = false
+		}
+
+		if e.IsDir() {
+			fmt.Fprintf(out, "%s%c───%s\n", preparedPrefix, hinge, e.Name())
+			newPath := filepath.Join(path, e.Name())
+			if err := dirTreeRecursive(out, newPath, printFiles, newLevels); err != nil {
+				return err
+			}
+		} else {
+			fileInfo, _ := e.Info()
+			var size string
+			if fileInfo.Size() == 0 {
+				size = "empty"
+			} else {
+				size = fmt.Sprintf("%db", fileInfo.Size())
+			}
+			fmt.Fprintf(out, "%s%c───%s (%s)\n", preparedPrefix, hinge, fileInfo.Name(), size)
+		}
+	}
+
+	return nil
+}
+
+func filter(elements []os.DirEntry, test func(os.DirEntry) bool) []os.DirEntry {
+	result := make([]os.DirEntry, 0)
+	for _, s := range elements {
+		if test(s) {
+			result = append(result, s)
+		}
+	}
+	return result
+}
